f866c9d3c42087826a36899d463d96ad7cce9fe9
Samu <samuelminana@gmail.com>
2026-02-09 20:32:31 +0100

    Implementacion de plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.samuelminana.tema4gradle"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.samuelminana.tema4gradle.Main"
+}
+
 
 java {
     toolchain {
