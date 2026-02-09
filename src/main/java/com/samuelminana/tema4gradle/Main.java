04e5c7427c9f05209d19826bb69587fd32d9521f
Samu <samuelminana@gmail.com>
2026-02-09 19:49:53 +0100

    Implementacion de historial de contexto

--- src/main/java/com/samuelminana/tema4gradle/Main.java
+++ src/main/java/com/samuelminana/tema4gradle/Main.java
@@ -1,17 +1,34 @@
 package com.samuelminana.tema4gradle;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        final String TOKEN = "ollama";
+
         var model = OpenAiChatModel.builder()
                 .baseUrl("http://localhost:11434/v1")
-                .apiKey("ollama")          // en local da igual
+                .apiKey(TOKEN)
                 .modelName("gemma:2b")
                 .build();
 
-        String respuesta = model.chat("Cuéntame un chiste");
-        System.out.println(respuesta);
+        List<ChatMessage> history = new ArrayList<>();
+
+        history.add(UserMessage.from("Hola, soy Samuel"));
+        AiMessage respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+
+        history.add(UserMessage.from("¿Recuerdas cómo me llamo?"));
+        respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+
+        System.out.println(respuesta.text());
     }
 }
